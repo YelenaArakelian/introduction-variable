98b15374899176ffe0f632c0c39ead8088b685d6
yelenaArakelian <yarakelian3@gmail.com>
2025-09-15 21:14:32 -0400

    made a canvas and circle in the center

--- js/script.js
+++ js/script.js
@@ -1,24 +1,27 @@
 /**
- * Title of Project
- * Author Name
- * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Introduction Variables
+ * Yelena Arakelian
+ *
+ * into to variables
+ *
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
+ * create canvas
+ */
 function setup() {
-
+  createCanvas(640, 640);
 }
 
-
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
+ * draw a circle in the center of the canvas
+ */
 function draw() {
-
+  push();
+  noStroke();
+  fill(255, 255, 0);
+  ellipse(320, 320, 100, 100);
+  pop();
 }
